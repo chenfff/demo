e82bcaf9e02642f2837fdefe5d0252950c594571
chenfeng <844474380@qq.com>
2022-11-15 21:52:01 +0800

    demo init

---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("demo")
+public class DemoController {
+
+
+    @GetMapping("index")
+    public String index(){
+        log.info("this is info log");
+        log.debug("this is debug log");
+        log.warn("this is warn log");
+        log.error("this is error log");
+        return "hello demo";
+    }
+}
